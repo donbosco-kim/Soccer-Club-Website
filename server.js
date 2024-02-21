c5e976637e28f902e980a9026a86bab09bc21993
Donbosco Kim <leonbosco2@gmail.com>
2024-02-21 15:04:09 -0600

    test localhost

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const expressLayouts = require("express-ejs-layouts");
+//const expressStatic = require('express-static')
 
-app.get('/', (req, res) => res.send('Heyy World!'))
+app.use(expressLayouts);
+app.set('layout', './layouts/layout')
+app.set("view engine", "ejs");
+
+//routes
+app.get('', (req, res) => {
+    res.render('home', {title: 'Home Page', layout: './layouts/layout'})
+})
+
+app.get('/team', (req, res) => {
+    res.render('team', {title: 'Team Page', layout: './layouts/layout'})
+})
+
+app.get('/news', (req, res) => {
+    res.render('news', {title: 'News Page', layout: './layouts/layout'})
+})
+
+//static files
+//app.use(express.static("public"));
+//app.use("/css", express.static(__dirname + "../public/"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
