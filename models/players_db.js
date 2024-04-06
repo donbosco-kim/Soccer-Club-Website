7531806f27358462d1c3f32deb8c4a89aaf32e1e
Donbosco Kim <leonbosco2@gmail.com>
2024-04-06 16:29:22 -0500

    correct the path to the database file

--- models/players_db.js
+++ models/players_db.js
@@ -4,7 +4,7 @@ const sqlite3 = require("sqlite3").verbose();
 // Function to open a connection to the database file
 function getDbConnection() {
   // Connecting to the database
-  let db = new sqlite3.Database("models/mizzousoccerdatabase.db",sqlite3.OPEN_READWRITE, (err) => {
+  let db = new sqlite3.Database("/Users/leonbosco/Desktop/Sqlite-Mizzou-SoccerDB/mizzousoccerdatabase.db",sqlite3.OPEN_READWRITE, (err) => {
       if (err) {
         console.error("You got this", err.message);
       }else {
