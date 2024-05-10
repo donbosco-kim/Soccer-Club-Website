e95d0a6283135dcd2d2d7fe53570da469448c18c
Donbosco Kim <leonbosco2@gmail.com>
2024-05-09 19:37:12 -0500

    Updated Readme and db.js
    - removed Frontend, back end
    - added aws hosting documentation uder documentation section
    - chaged the database path in db.js from absolute to relative

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 # Soccer-Club-Website
 
-This is a capstone project made by team 10 (Matthew Hartmann, Noah Reeves, Downing Abbot, Donbosco Kim, Grant Newsham). It is still in progress...
-
-## Frontend
-
-## Backend
-
-## Server (hosting & Domain)
-Add a documentation for using AWS hosting (In Progress)
-- [Hosting](/docs/server-hosting.md)
+This is a capstone project made by team 10 (Matthew Hartmann, Noah Reeves, Downing Abbot, Donbosco Kim, Grant Newsham).
 
 ## Documentation
 
 All documentation is in this /docs folder.
-
+<!-- - [Frontend](/docs/frontend.md)
+- [Backend](/docs/backend.md) -->
 - [Database](/docs/database.md)
+- [AWS Hosting](/docs/server-hosting.md)
--- models/db.js
+++ models/db.js
@@ -1,9 +1,11 @@
-// import necessary modules
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
 try {
-  // Connecting to the database
-  const db = new sqlite3.Database("/Users/leonbosco/Desktop/Sqlite-Mizzou-SoccerDB/mizzousoccerdatabase.db", sqlite3.OPEN_READWRITE, (err) => {
+  const dbPath = path.resolve(__dirname, "../var/db/mizzousoccerdatabase.db");
+  console.log(dbPath);
+
+  const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
       throw new Error("Error connecting to the database: " + err.message);
     } else {
@@ -11,9 +13,16 @@ try {
     }
   });
 
-  module.exports = db; // Export the database connection
+  module.exports = db;
 } catch (error) {
   console.error("An error occurred while opening the database:", error.message);
-  throw error; // Throw the error for further handling by the caller
+  throw error;
 }
+// mv /Users/leonbosco/Desktop/Sqlite-Mizzou-SoccerDB/mizzousoccerdatabase.db /
+
+// sudo scp /Users/leonbosco/Desktop/Sqlite-Mizzou-SoccerDB/mizzousoccerdatabase.db ubuntu@3.16.254.68:/home/ubuntu/Soccer-Club-Website/var/db
+
+// scp -i "nodejs-ssl-key.pem" /Users/leonbosco/Desktop/Sqlite-Mizzou-SoccerDB/mizzousoccerdatabase.db ubuntu@3.16.254.68:/home/ubuntu/Soccer-Club-Website/var/db
+
+
 
